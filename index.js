12fcdb081227ecc2b92f039694f5b4d07bd221a0
Sean Nekouei <sean.nekouei@gmail.com>
2022-10-25 13:53:53 -0600

    commit tree c3a750d496f679f43ceb6cfa1082840b53315079

---
+++ index.js
@@ -0,0 +1,42 @@
+function add() {
+
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+
+}
+function multiply(a, b) {
+    return a * b;
+
+}
+function divide(a, b) {
+    return a / b;
+
+}
+function makeInt(string) {
+    console.log(string)
+    return parseInt(string, 10)
+}
+function preserveDecimal(string) {
+    console.log(string)
+    return parseFloat(string)
+}
+function increment(number) {
+    return (number += 1) 
+}
+function decrement(number) {
+    return (number -= 1) 
+}
+
